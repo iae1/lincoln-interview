5178b2d5889e44e3bf46c0322df3e1c4f70cf930
Isaac Easton <iae1@protonmail.com>
2021-07-14 16:03:25 -0400

    FE: Builds out ReactJS structure

---
+++ client/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Routes from "./routes";
+
+export default function App () {
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+    </div>
+  );
+};
+
+
---
+++ client/routes.js
@@ -0,0 +1,14 @@
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Upload from "./components/Upload"
+import Donations from "./components/Donations"
+
+export default function Routes () {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/donations" component={Donations}/>
+            <Route exact path="/upload" component={Upload}/>
+        </Switch>
+    )
+}
