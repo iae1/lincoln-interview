29ddf6a524769ce40d2b80c39fd26e3250a87625
Isaac Easton <iae1@protonmail.com>
2021-07-15 01:59:06 -0400

    redoes sequelize config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,45 @@
-const env = require('./env.js');
- 
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
-  operatorsAliases: false,
+// const sequelize = new Sequelize(env.database, env.username, env.password, {
+//   host: env.host,
+//   dialect: env.dialect,
+//   operatorsAliases: false,
  
-  pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
+//   pool: {
+//     max: env.max,
+//     min: env.pool.min,
+//     acquire: env.pool.acquire,
+//     idle: env.pool.idle
+//   }
+// });
+
+const pkg = require('../../package.json')
+
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-});
+} else {
+  config = {
+    logging: false
+  }
+}
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+)
  
 const db = {};
  
